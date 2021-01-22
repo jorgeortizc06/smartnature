b9e339d073987fa54b8bfb6327f702603ebdf00f
jorgeortizc05 <jorgeortizc05@gmail.com>
2021-01-21 21:22:49 -0500

    add procfile, reque_38.txt, gunicorn and dj-static for heroku gunicorn: desplega mi web server dj-static: establece mi ruta de archivos estaticos procfile: archivo de configuracion para heroku reque_38.txt: librerias anteriores descritas

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application() #funciona solo en local
+application = Cling(get_wsgi_application()) #Uso la libreria pip install dj-static que me permite determinar mis archivos estaticos cuando paso a produccion
+#tambien se usa pip install gunicorn, esta linea me permite ubicar los archivos estaticos de mi proyecto para que funcione
