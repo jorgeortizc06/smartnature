069c81cdc346d458904c0209cad467aad90061cd
jorgeortizc05 <jorgeortizc05@gmail.com>
2021-02-23 22:19:14 -0500

    consumo de agua con ajax, calcula el total de agua consumido por mes

--- gestion_riego/views/historial_riego/historial_riego_view.py
+++ gestion_riego/views/historial_riego/historial_riego_view.py
@@ -60,13 +60,39 @@ class UsoAguaPorTipoLogicaDifusa(TemplateView):
 
     def post(self, request, *args, **kwargs):
         data = {}
+        try:
+            action = request.POST['action']
+            if action == 'search_consumo_agua_month':
+                fecha_ajax = request.POST['fecha']
 
-    def get_graph_historial_riego_month(self, mes, anio):
+                fecha = datetime.strptime(fecha_ajax, '%Y-%m-%d')  # convierto a fecha para que python lo entienda
+                formateo_fecha = datetime.strftime(fecha, '%B')  # ne sirve para visualizarlo en el chart
+                dia = fecha.day
+                mes = fecha.month
+                anio = fecha.year
+                data = []
+                data_consumo_agua_month= self.get_graph_historial_consumo_agua_month(mes, anio)
+                data_consumo_agua_mensual_per_tipo_logica_difusa = self.get_consumo_agua_month_total(mes,anio)
+                data = {'consumo_agua_1_variable': data_consumo_agua_month['data_consumo_agua_1_variable_mensual'],
+                        'consumo_agua_3_variable': data_consumo_agua_month['data_consumo_agua_3_variable_mensual'],
+                        'consumo_agua_4_variable': data_consumo_agua_month['data_consumo_agua_4_variable_mensual'],
+                        'consumo_agua_1_variable_mensual': data_consumo_agua_mensual_per_tipo_logica_difusa[0]['consumo_agua_1_variable_mensual'],
+                        'consumo_agua_3_variable_mensual': data_consumo_agua_mensual_per_tipo_logica_difusa[0]['consumo_agua_3_variable_mensual'],
+                        'consumo_agua_4_variable_mensual': data_consumo_agua_mensual_per_tipo_logica_difusa[0]['consumo_agua_4_variable_mensual'],
+                        'mes': formateo_fecha}
+            else:
+                data['error'] = 'Ha ocurrido un error'
+        except Exception as e:
+            data['error'] = str(e)
+        return JsonResponse(data, safe=False)
+
+    def get_historial_consumo_agua_month(self, mes, anio):
         data_historial_riego_1_variable = []
         data_historial_riego_3_variable = []
         data_historial_riego_4_variable = []
 
         data = []
+        consumo_agua_mensual = []
         riego = {}
         try:
             for d in range(1, 32):
@@ -89,10 +115,73 @@ class UsoAguaPorTipoLogicaDifusa(TemplateView):
                 data.append({'dia': d,'data_historial_riego_1_variable': float(sum_riego_1_variable), 'consumo_agua_1_variable': float(consumo_agua_1_variable),
                              'data_historial_riego_3_variable': float(sum_riego_3_variable), 'consumo_agua_3_variable': float(consumo_agua_3_variable),
                              'data_historial_riego_4_variable': float(sum_riego_4_variable), 'consumo_agua_4_variable': float(consumo_agua_4_variable)})
+            #print(data)
+        except:
+            pass
+        return data
 
+    def get_consumo_agua_month_total(self,mes,anio):
+        data = []
 
+        try:
+            print("-----------------CONSUMO DE AGUA MENSUAL------------------")
+            sum_riego_1_variable_mensual = HistorialRiego.objects.filter(fecha_riego__month=mes,
+                                                                         fecha_riego__year=anio, codigo_sensor=5).aggregate(
+                r=Coalesce(Sum('tiempo_riego_1_variable'), 0)).get('r')
+            consumo_agua_1_variable_mensual = round(float(sum_riego_1_variable_mensual) * 2.03, 2)
+            print(consumo_agua_1_variable_mensual)
+            sum_riego_3_variable_mensual = HistorialRiego.objects.filter(fecha_riego__month=mes,
+                                                                         fecha_riego__year=anio, codigo_sensor=5).aggregate(
+                r=Coalesce(Sum('tiempo_riego'), 0)).get('r')
+            consumo_agua_3_variable_mensual = round(float(sum_riego_3_variable_mensual) * 2.03, 2)
+            print(consumo_agua_3_variable_mensual)
+            sum_riego_4_variable_mensual = HistorialRiego.objects.filter(fecha_riego__month=mes,
+                                                                         fecha_riego__year=anio, codigo_sensor=5).aggregate(
+                r=Coalesce(Sum('tiempo_riego_4_variable'), 0)).get('r')
+            consumo_agua_4_variable_mensual = round(float(sum_riego_4_variable_mensual) * 2.03, 2)
+            print(consumo_agua_4_variable_mensual)
+            data.append({'consumo_agua_1_variable_mensual': float(consumo_agua_1_variable_mensual),
+                        'consumo_agua_3_variable_mensual': float(consumo_agua_3_variable_mensual),
+                        'consumo_agua_4_variable_mensual': float(consumo_agua_4_variable_mensual)})
+            #print(data[0]['consumo_agua_1_variable_mensual'])
+            for p in data:
+                print(p['consumo_agua_1_variable_mensual'])
+        except:
+            pass
+        return data
 
-            print(data)
+    def get_graph_historial_consumo_agua_month(self,mes,anio):
+        data_consumo_agua_1_variable_mensual= []
+        data_consumo_agua_3_variable_mensual = []
+        data_consumo_agua_4_variable_mensual = []
+        data={}
+
+        try:
+            for d in range(1, 32):
+                sum_riego_1_variable = HistorialRiego.objects.filter(fecha_riego__day=d, fecha_riego__month=mes,
+                                                                     fecha_riego__year=anio, codigo_sensor=5).aggregate(
+                    r=Coalesce(Sum('tiempo_riego_1_variable'), 0)).get('r')
+                print(sum_riego_1_variable)
+                consumo_agua_1_variable = round(float(sum_riego_1_variable) * 2.03, 2)
+
+                sum_riego_3_variable = HistorialRiego.objects.filter(fecha_riego__day=d, fecha_riego__month=mes,
+                                                                     fecha_riego__year=anio, codigo_sensor=5).aggregate(
+                    r=Coalesce(Sum('tiempo_riego'), 0)).get('r')
+                consumo_agua_3_variable = round(float(sum_riego_3_variable) * 2.03, 2)
+
+                sum_riego_4_variable = HistorialRiego.objects.filter(fecha_riego__day=d, fecha_riego__month=mes,
+                                                                     fecha_riego__year=anio, codigo_sensor=5).aggregate(
+                    r=Coalesce(Sum('tiempo_riego_4_variable'), 0)).get('r')
+                consumo_agua_4_variable = round(float(sum_riego_4_variable) * 2.03, 2)
+                data_consumo_agua_1_variable_mensual.append(float(consumo_agua_1_variable))
+                data_consumo_agua_3_variable_mensual.append(float(consumo_agua_3_variable))
+                data_consumo_agua_4_variable_mensual.append(float(consumo_agua_4_variable))
+
+            data = {
+                'data_consumo_agua_1_variable_mensual': data_consumo_agua_1_variable_mensual,
+                'data_consumo_agua_3_variable_mensual': data_consumo_agua_3_variable_mensual,
+                'data_consumo_agua_4_variable_mensual': data_consumo_agua_4_variable_mensual,
+            }
 
         except:
             pass
@@ -101,10 +190,18 @@ class UsoAguaPorTipoLogicaDifusa(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         #data_historial_riego_month = self.get_graph_historial_riego_month(mes = datetime.now().month, anio = datetime.now().year)
-        data_historial_riego_month = self.get_graph_historial_riego_month(mes = 12, anio = 2020)
+        data_historial_consumo_agua_month = self.get_historial_consumo_agua_month(mes = 12, anio = 2020)
+        data_consumo_agua_mensual_per_tipo_logica_difusa = self.get_consumo_agua_month_total(mes = datetime.now().month, anio = datetime.now().year)
+        data_consumo_agua = self.get_graph_historial_consumo_agua_month(mes = datetime.now().month, anio = datetime.now().year)
         context['title'] = 'Consumo de Agua'
         context['entity'] = 'Device'
-        context['consumo_agua'] = data_historial_riego_month
+        context['mes'] = datetime.now().strftime('%B')
+        context['consumo_agua_1_variable'] = data_consumo_agua['data_consumo_agua_1_variable_mensual']
+        context['consumo_agua_3_variable'] = data_consumo_agua['data_consumo_agua_3_variable_mensual']
+        context['consumo_agua_4_variable'] = data_consumo_agua['data_consumo_agua_4_variable_mensual']
+        context['consumo_agua'] = data_historial_consumo_agua_month
+        context['consumo_agua_total'] = data_consumo_agua_mensual_per_tipo_logica_difusa
+
 
         return context
 
