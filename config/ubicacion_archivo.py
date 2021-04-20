8e1e5197c4f90eb17059c4ce7edc74ca02747500
jorgeortizc05@gmail.com <jorgeortizc05@gmail.com>
2021-04-20 18:54:51 -0500

    change ip add for IoT Database

--- config/ubicacion_archivo.py
+++ config/ubicacion_archivo.py
@@ -1,7 +1,7 @@
-ip_db = 'localhost' #En centos no funciona localhost, poner la ip directamente
+ip_db = '192.168.0.253' #En centos no funciona localhost, poner la ip directamente
 imagen_fuzzy1 = "/casaortiz/smartnature/gestion_riego/media/fuzzy1/"
 imagen_fuzzy3 = "/casaortiz/smartnature/gestion_riego/media/fuzzy3/"
 imagen_fuzzy4 = "/casaortiz/smartnature/gestion_riego/media/fuzzy4/"
 
-ip_mqtt_broker = "192.168.100.254"
+ip_mqtt_broker = "192.168.0.254"
 port_mqtt = 1883
